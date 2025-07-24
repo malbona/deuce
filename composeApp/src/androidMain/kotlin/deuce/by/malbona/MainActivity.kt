c67afcce9d500b426eb247c022ec9625b358d33f
malbona <41956963+malbona@users.noreply.github.com>
2025-07-24 09:20:28 +0800

    feat: Set landscape orientation and hide status bar
    This commit updates the `MainActivity` to:
    - Set the screen orientation to landscape.
    - Hide the status bar.
    - Use `enableEdgeToEdge()` from `androidx.activity`.

--- composeApp/src/androidMain/kotlin/deuce/by/malbona/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/deuce/by/malbona/MainActivity.kt
@@ -1,17 +1,23 @@
 package deuce.by.malbona
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
         setContent {
             App()
         }
